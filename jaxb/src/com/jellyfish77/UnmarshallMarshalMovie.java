49b6376273ad01a99d5c29334551b4945b35d0ef
jellyfish77 <jellyfish77@gmx.com>
2019-06-23 13:52:05 +0800

    add marshal functionality #wip

--- jaxb/src/com/jellyfish77/UnmarshallMarshalMovie.java
+++ jaxb/src/com/jellyfish77/UnmarshallMarshalMovie.java
@@ -1,19 +1,23 @@
 package com.jellyfish77;
 
 import java.io.File;
+import java.io.PrintStream;
 import java.util.List;
 
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
+import javax.xml.bind.Marshaller;
 import javax.xml.bind.Unmarshaller;
 
 import com.jellyfish77.Movie.Genres;
 
-public class XmlToObject {
+public class UnmarshallMarshalMovie {
 	public static void main(String[] args) {
 
 		try {
-
+			
+			// unmarshall Movie XML (convert XML to Obj)
+			
 			File file = new File("data/com/jellyfish77/Movie.xml");
 			JAXBContext jaxbContext = JAXBContext.newInstance(Movie.class);
 
@@ -26,6 +30,12 @@ public class XmlToObject {
 			Genres genres = que.getGenres();
 			genres.getGenre().forEach((g)->System.out.println(g));			
 			
+			// marshall Movie Obj (convert Obj to XML)
+			
+			Marshaller marshallerObj = jaxbContext.createMarshaller();
+			marshallerObj.setProperty(Marshaller.JAXB_FORMATTED_OUTPUT, true);
+			
+			marshallerObj.marshal(que, new PrintStream(System.out));
 
 		} catch (JAXBException e) {
 			e.printStackTrace();
